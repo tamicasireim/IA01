3342371a0adcebe21df8f3d9af8bf4feddd1e909
tamicasireim <msimatic.utc@gmail.com>
2015-10-16 14:59:44 +0200

    maj TP2 + ajout début TD5

--- TP2/tp2.lisp
+++ TP2/tp2.lisp
@@ -1,13 +1,17 @@
 (defun l ()
   (load "tp2.lisp"))
 
-(setq notreliste '(A B C D))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; FONCTIONS UTILITAIRES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;renvoie le l'élément à la position pos de la liste (similaire à nth)
 (defun nelem (liste pos)
   (if (eq pos 1)
     (car liste)
     (nelem (cdr liste) (- pos 1))))
 
+;échange les éléments aux positions pos1 et pos2 d'une liste. (echange '(A B C D) 1 2) renvoie (B A C D)
 (defun echange (liste pos1 pos2)
   (let ((pos_tmp 0) (retour nil))
     (dolist (elem liste retour)
@@ -16,74 +20,138 @@
 	    ((eq pos_tmp pos2) (setq retour (append retour (list (nelem liste pos1)))))
 	    (T (setq retour (append retour (list elem))))))))
 
-(defun allowed_state (liste)
-  (if (eq nil (member 'A (member 'D liste)))
-    T
-    nil))
 
-(defun successeurs (etat)
-  (let ((retour nil))
-    (if (allowed_state (echange etat 1 2))
-      (setq retour (list (echange etat 1 2))))
-    (if (allowed_state (echange etat 2 3))
-      (setq retour (append retour (list (echange etat 2 3)))))
-    (if (allowed_state (echange etat 2 4))
-      (setq retour (append retour (list (echange etat 2 4))))
-      retour)))
-
-;fonction recherche (etatA etatR profondeur chemin)
-;si etatA = etatR
-;	retourner chemin
-;sinon
-;	si profondeur = 0
-;		retourner NIL
-;	sinon
-;		pour chaque etat de successeurs(etatA)
-;			nouveauchemin := append(chemin, n° état)
-;			r := recherche(etat etatR (profondeur -1) nouveauchemin)
-;			si r != NIL retourner r 
-;		fin pour
-;		pour chaque etat de successeurs(etatA)
-;			nouveauchemin := append(chemin, n° état)
-;			r := recherche(etat etatR profondeur nouveauchemin)
-;			si r != NIL retourner r 
-;		fin pour
-;		
-(defun size (liste)
-  (if (eq nil liste)
-    0
-    (+ 1 (size (cdr liste)))))
-
-(defun avant_dernier (liste)
-  (if (> (size liste) 1)
-    (nelem liste (- (size liste) 1))
-    (car liste)))
-
+;équivalent à la fonction member native de common lisp, mais gère le cas des liste de listes.
 (defun is_member (item liste)
   (cond ((eq liste nil) NIL)
 	((equal item (car liste)) T)
 	(T (is_member item (cdr liste)))))
 
-(defun recherche (etatA etatR)
-  (recherche_a etatA etatR 3 (list etatA)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; FONCTIONS POUR FACILITER LA MANIPULATION DES ETATS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun recherche_a (etatA etatR profondeur chemin)
-  (cond ((equal etatA etatR) chemin)
-	((eq profondeur 0) NIL)
-	(T (dolist (etat (successeurs etatA) NIL)
-	     (if (not (is_member etat chemin))
-	       (let ((nouveauchemin (append chemin (list etat))))
-		 ;(print nouveauchemin)
-		 (let ((research (recherche_a etat etatR (- profondeur 1) nouveauchemin)))
-		   (if (not (equal research NIL))
-		     (return-from recherche_a research))))
-	       ))
-	   (dolist (etat (successeurs etatA) NIL)
-	     (if (not (is_member etat chemin))
-	       (let ((nouveauchemin (append chemin (list etat))))
-		 ;(print nouveauchemin)
-		 (let ((research (recherche_a etat etatR profondeur nouveauchemin)))
-		   (if (not (equal research NIL))
-		     (return-from recherche_a research))))
-		 ))
-	   )))
+;renvoie T si la liste est un état adéquat c'est à dire s'il n'y a pas de A après D et si ce n'est pas un état qui ait déjà été visité.
+(defun allowed_state (etat etats_visites)
+  (if (and (eq nil (member 'A (member 'D etat))) (not (is_member etat etats_visites)))
+    T
+    nil))
+
+;Renvoie la liste des successeurs valides d'un état.
+(defun successeurs (etat etats_visites)
+  (let ((retour nil))
+    (if (allowed_state (echange etat 1 2) etats_visites)
+      (setq retour (list (echange etat 1 2))))
+    (if (allowed_state (echange etat 2 3) etats_visites)
+      (setq retour (append retour (list (echange etat 2 3)))))
+    (if (allowed_state (echange etat 2 4) etats_visites)
+      (setq retour (append retour (list (echange etat 2 4)))))
+      retour))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Algorithme de recherche en profondeur d'abord
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;renvoie le chemin pour aller de l'état actuel à l'état recherché
+(defun recherche_prof (etatActuel etatRecherche etatsVisites)
+  (let ((NetatsVisites (append etatsVisites (list etatActuel))))
+    (cond ((equal etatActuel etatRecherche) NetatsVisites)
+	  (T (let ((succs (successeurs etatActuel NetatsVisites)))
+		 (dolist (succ succs)
+		   (let ((chemin (recherche_prof succ etatRecherche NetatsVisites)))
+		     (when chemin
+		       (return-from recherche_prof chemin)
+		       )
+		     )
+		   )
+	       )
+	     )
+	  )
+    )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ALGORITHME DE RECHERCHE SELON UNE HEURISTIQUE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; FONCTIONS DE CALCUL POUR L'HEURISTIQUE
+
+;renvoie le nombre d'éléments en commun dans une liste
+(defun lettre_commun (etat1 etat2)
+  (let ((retour 0))
+    (mapcar (lambda (x y)
+	      (if (eq x y)
+		(incf retour 1)
+		)
+	      )
+	    etat1 etat2
+	    )
+    retour
+    )
+  )
+
+; détermine le nombre d'états successeurs à un état donné
+(defun nombre_successeurs (etat etatsVisites)
+  (length (successeurs2 etat etatsVisites)))
+
+; détermine si les successeurs d'un état sont solutions ou non
+(defun is_solution (etat etatRecherche etatsVisites)
+  (is_member etatRecherche (successeurs2 etat etatsVisites)))
+
+;;; HEURISTIQUE ET EXPLOITATION
+
+; calcul de l'heuristique = nombre de lettre en commun + nombre de successeur
+(defun heuristique (etat etatRecherche etatsVisites)
+  (let ((score 0))
+    (if (is_solution etat etatRecherche etatsVisites)
+      (incf score 1000)
+      (progn (incf score (lettre_commun etat etatRecherche))
+	     (incf score (nombre_successeurs etat etatsVisites)))
+      )
+    )
+  )
+
+; choisi l'état le plus prometteur selon l'heuristique
+(defun choixEtat (listeEtats etatRecherche etatsVisites)
+  (let ((meilleur_choix nil)
+	(maximum 0)
+	(pos 1)
+	(pos_max 0)
+	)
+    (dolist (etat listeEtats)
+      (if (> (heuristique etat etatRecherche etatsVisites) maximum)
+	(progn (setq maximum (heuristique etat etatRecherche etatsVisites))
+	       (setq pos_max pos))
+	)
+      (incf pos 1)
+      )
+    (nelem listeEtats pos_max)
+    )
+  )
+; classe les états selon leur pertinence d'après l'heuristique.
+(defun classementEtat (listeEtats etatRecherche etatsVisites)
+  (if (eq listeEtats nil)
+    nil
+    (append (list (choixEtat listeEtats etatRecherche etatsVisites))
+	    (classementEtat (remove (choixEtat listeEtats etatRecherche etatsVisites) listeEtats)
+			    etatRecherche etatsVisites)
+	    )
+    )
+  )
+
+;;; ALGORITHME DE RECHERCHE USANT DE L'HEURISTIQUE:w
+
+(defun recherche_h (etatActuel etatRecherche etatsVisites)
+  (let ((NetatsVisites (append etatsVisites (list etatActuel))))
+    (cond ((equal etatActuel etatRecherche) NetatsVisites)
+	  (T (let ((classement (classementEtat (successeurs2 etatActuel etatsVisites) etatRecherche etatsVisites)))
+		 (dolist (succ classement)
+		   (let ((chemin (recherche_h succ etatRecherche NetatsVisites)))
+		     (when chemin
+		       (return-from recherche_h chemin)
+		       )
+		     )
+		   )
+	       )
+	     )
+	  )
+    )
+  )
